b5c0629b5dd26e1b26ecca512d2c58ad29766ffa
Sofiia <e2467447@cmaisonneuve.qc.ca>
2026-02-17 09:26:48 -0500

    premiere validation

--- Collision des voitures.py
+++ Collision des voitures.py
@@ -1,2 +1,10 @@
-def main():
-    print('Hello')
+#Voiture1
+vitesse_initiale1 = input("Entrer la vitesse initiale du premier véhicule:")
+masse_voiture1 =
+
+
+
+
+
+#Voiture2
+vitesse_initiale2 = input("Entrer la vitesse intiale du deuxième véhicule:")
