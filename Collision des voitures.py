e3f006d57c6904366dccf93a7ab582b2ff6f72e3
Sofiia <e2467447@cmaisonneuve.qc.ca>
2026-02-17 10:24:28 -0500

    2

--- Collision des voitures.py
+++ Collision des voitures.py
@@ -1,11 +1,12 @@
 #Voiture1
 vitesse_initiale1 = input("Entrer la vitesse initiale du premier véhicule:")
-masse_voiture1 =
-
-
-
+masse_voiture1 = input("Entrer la masse du premier vehicule:")
+position_x_1 = input("Indiquer la position en x entre -20m et 20m du premier vehicule:") #Attention lorsque la position ne rentre pas dans les restrictions: Afficher erreur.
+position_y_1 = input("Indiquer la position en y entre -20m et 20m du premier vehicule:")
 
 
 #Voiture2
 vitesse_initiale2 = input("Entrer la vitesse intiale du deuxième véhicule:")
-fgj
+masse_voiture2 = input("Entrer la masse du deuxième vehicule:")
+position_x_2 = input("Indiquer la position en x entre -20m et 20m du deuxième vehicule:")
+position_y_2 = input("Indiquer la position en y entre -20m et 20m du deuxième vehicule:")
