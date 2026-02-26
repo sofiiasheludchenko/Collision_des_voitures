a67c495c776c1446e2afc848f8b088710eed56c0
Dylan123656 <e6303109@cmaisonneuve.qc.ca>
2026-02-26 13:38:33 -0500

    Update Collision des voitures.py

--- Collision des voitures.py
+++ Collision des voitures.py
@@ -1,12 +1,37 @@
 #Voiture1
-vitesse_initiale1 = input("Entrer la vitesse initiale du premier véhicule:")
-masse_voiture1 = input("Entrer la masse du premier vehicule:")
-position_x_1 = input("Indiquer la position en x entre -20m et 20m du premier vehicule:") #Attention lorsque la position ne rentre pas dans les restrictions: Afficher erreur.
-position_y_1 = input("Indiquer la position en y entre -20m et 20m du premier vehicule:")
+vitesse_initiale1 = int(input("Entrer la vitesse (m/s) initiale du premier véhicule:"))
+masse_voiture1 = int(input("Entrer la masse (kg) du premier vehicule:"))
+
+while True: #le loop fait en sorte que si l'utilisateur met une valeur hors de -20 et 20, ca affiche un message d'erreur tant qu'il met une valeur valide
+  position_x_1 = int(input("Indiquer la position de la voiture 1 en x entre -20m et 20m:")) #Attention lorsque la position ne rentre pas dans les restrictions: Afficher erreur.
+  if -20 <= position_x_1 <= 20:
+    break
+  else:
+    print("Erreur: La position de la voiture 1 en x doit être entre -20m et 20m")
+
+while True:
+  position_y_1 = int(input("Indiquer la position de la voiture 1 en y entre -20m et 20m:"))
+  if -20 <= position_y_1 <= 20:
+    break
+  else:
+    print("Erreur: La position de la voiture 1 en y doit être entre -20m et 20m")
+  
 
 
 #Voiture2
-vitesse_initiale2 = input("Entrer la vitesse intiale du deuxième véhicule:")
-masse_voiture2 = input("Entrer la masse du deuxième vehicule:")
-position_x_2 = input("Indiquer la position en x entre -20m et 20m du deuxième vehicule:")
-position_y_2 = input("Indiquer la position en y entre -20m et 20m du deuxième vehicule:")
+vitesse_initiale2 = int(input("Entrer la vitesse (m/s) intiale du deuxième véhicule:"))
+masse_voiture2 = int(input("Entrer la masse (kg) du deuxième vehicule:"))
+
+while True:
+  position_x_2 = int(input("Indiquer la position de la voiture 2 en x entre -20m et 20m:"))
+  if -20 <= position_x_2 <= 20:
+    break
+  else:
+    print("Erreur: La position de la voiture 2 en x doit être entre -20m et 20m")
+
+while True:
+  position_y_2 = int(input("Indiquer la position de la voiture 2 en y entre -20m et 20m du deuxième vehicule:"))
+  if -20 <= position_y_2 <= 20:
+    break
+  else:
+    print("Erreur: La position de la voiture 2 en y doit être entre -20m et 20m")
