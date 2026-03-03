208a45fa1ced82bd9416b4a1c49e0916ebc72f53
sofiiasheludchenko <e2467447@cmaisonneuve.qc.ca>
2026-03-03 10:13:57 -0500

    Update Collision des voitures.py

--- Collision des voitures.py
+++ Collision des voitures.py
@@ -1,5 +1,7 @@
+
 import pandas as pd
 import tkinter as tk
+import numpy as np
 
 # ==============================Voiture1==============================
 
@@ -13,7 +15,7 @@ while True:
 
 # ------------------------------vitesses 1------------------------------
 vitesse_initiale1 = float(input("Entrer la vitesse initiale (m/s) du premier véhicule:"))
-#vitesse_finale = float(input("Entrer la vitesse finale (m/s) souhaité du vehicule 1:"))
+position_finale1= float(input("Entrer la position finale (m) souhaité du vehicule 1:"))
 
 # ------------------------------positions 1 en x------------------------------
 while True:  # le loop fait en sorte que si l'utilisateur met une valeur hors de -20 et 20, ca affiche un message d'erreur tant qu'il met une valeur valide
@@ -57,7 +59,7 @@ while True:
 
 # ------------------------------vitesses 2------------------------------
 vitesse_initiale2 = float(input("Entrer la vitesse intiale (m/s) du deuxième véhicule:"))
-#vitesse_finale = float(input("Entrer la vitesse finale (m/s) souhaité du vehicule 2:"))
+position_finale2 = float(input("Entrer la position finale (m) souhaité du vehicule 2:"))
 
 # ------------------------------positions 2 en x------------------------------
 while True:
@@ -95,7 +97,7 @@ while True:
 donnes_dict = {
     'Vitesse initiale (m)': [vitesse_initiale1, vitesse_initiale2],
     'Position vers la quelle la voiture se dirigeait (m)': [position_finale1, position_finale2],
-    'Position finale des vehicules apres la collision(m)' : []
+    'Position finale des vehicules apres la collision(m)' : [],
     'Vitesse finale des vehicules apres la collision (m)': [0,0], #parfaitement inelastique (pour le sprint 1)
     'Masse (kg)': [masse_voiture1, masse_voiture2],
 }
@@ -127,6 +129,5 @@ for i in range(n_steps):
 
 if not collision_detectee:
     print("\n Aucune collision détectée sur la trajectoire.")
-
 
 
