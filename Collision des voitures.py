66c5b7cee96b6b83a165d8ebb77d8b58883d9836
sofiiasheludchenko <e2467447@cmaisonneuve.qc.ca>
2026-03-03 10:58:55 -0500

    Update Collision des voitures.py

--- Collision des voitures.py
+++ Collision des voitures.py
@@ -15,7 +15,6 @@ while True:
 
 # ------------------------------vitesses 1------------------------------
 vitesse_initiale1 = float(input("Entrer la vitesse initiale (m/s) du premier véhicule:"))
-position_finale1= float(input("Entrer la position finale (m) souhaité du vehicule 1:"))
 
 # ------------------------------positions 1 en x------------------------------
 while True:  # le loop fait en sorte que si l'utilisateur met une valeur hors de -20 et 20, ca affiche un message d'erreur tant qu'il met une valeur valide
@@ -59,7 +58,6 @@ while True:
 
 # ------------------------------vitesses 2------------------------------
 vitesse_initiale2 = float(input("Entrer la vitesse intiale (m/s) du deuxième véhicule:"))
-position_finale2 = float(input("Entrer la position finale (m) souhaité du vehicule 2:"))
 
 # ------------------------------positions 2 en x------------------------------
 while True:
@@ -92,17 +90,6 @@ while True:
     else:
         print("ERREUR: La position finale (m) de la voiture 2 en y doit être entre -20m et 20m")
 
-#------------------------------creation du tableau comparatif------------------------------
-
-donnes_dict = {
-    'Vitesse initiale (m)': [vitesse_initiale1, vitesse_initiale2],
-    'Position vers la quelle la voiture se dirigeait (m)': [position_finale1, position_finale2],
-    'Position finale des vehicules apres la collision(m)' : [],
-    'Vitesse finale des vehicules apres la collision (m)': [0,0], #parfaitement inelastique (pour le sprint 1)
-    'Masse (kg)': [masse_voiture1, masse_voiture2],
-}
-df_donnes_collision_voitures = pd.DataFrame(donnes_dict, index=['Vehicule 1', 'Vehicule 2'])
-print(df_donnes_collision_voitures)
 
 # vérification de collisions
 #==============================Collision sur trajectoire==============================
@@ -124,10 +111,22 @@ for i in range(n_steps):
     distance = np.sqrt((x_traj_1[i] - x_traj_2[i])**2 + (y_traj_1[i] - y_traj_2[i])**2)
     if distance <= tolerance:
         collision_detectee = True
-        print(f"\n Collision détectée ! Les voitures se rencontrent à environ x={x_traj_1[i]:.2f} m, y={y_traj_1[i]:.2f} m")
+        print("\n Collision détectée !")
         break
 
 if not collision_detectee:
     print("\n Aucune collision détectée sur la trajectoire.")
 
+#------------------------------creation du tableau comparatif------------------------------
+
+donnes_dict = {
+    'Vitesse initiale (m)': [vitesse_initiale1, vitesse_initiale2],
+    'Trajectoire (m)': [positionfinale_x_1, positionfinale_x_2],
+    "Position initiale (m)": [position_x_1, position_x_2],
+    'Position finale (m)' : [x_traj_1[i],y_traj_1[i]],
+    'Vitesse finale des vehicules apres la collision (m)': [0,0], #parfaitement inelastique (pour le sprint 1)
+    'Masse (kg)': [masse_voiture1, masse_voiture2],
+}
+df_donnes_collision_voitures = pd.DataFrame(donnes_dict, index=['Vehicule 1', 'Vehicule 2'])
+print(df_donnes_collision_voitures.round(2).to_string())
 
