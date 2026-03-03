9e2ce8dffcc8870333566a92e47fc6b24d2d0525
sofiiasheludchenko <e2467447@cmaisonneuve.qc.ca>
2026-03-03 09:08:30 -0500

    Update Collision des voitures.py

--- Collision des voitures.py
+++ Collision des voitures.py
@@ -1,36 +1,58 @@
-#Voiture1
+
+# Voiture1
 vitesse_initiale1 = int(input("Entrer la vitesse (m/s) initiale du premier véhicule:"))
 masse_voiture1 = int(input("Entrer la masse (kg) du premier vehicule:"))
+acceleration_vehicule1 = int(input("Entrer l'acceleration du vehicule 1:"))
+vitesse_finale = int(input("Entrer la vitesse finale souhaité du vehicule 1:"))
 
-while True: #le loop fait en sorte que si l'utilisateur met une valeur hors de -20 et 20, ca affiche un message d'erreur tant qu'il met une valeur valide
-  position_x_1 = int(input("Indiquer la position de la voiture 1 en x entre -20m et 20m:")) #Attention lorsque la position ne rentre pas dans les restrictions: Afficher erreur.
-  if -20 <= position_x_1 <= 20:
-    break
-  else:
-    print("Erreur: La position de la voiture 1 en x doit être entre -20m et 20m")
+while True:  # le loop fait en sorte que si l'utilisateur met une valeur hors de -20 et 20, ca affiche un message d'erreur tant qu'il met une valeur valide
+    positionfinale_x_1 = int(input(
+        "Indiquer la position finale de la voiture 1 en x entre -20m et 20m:"))  # Attention lorsque la position ne rentre pas dans les restrictions: Afficher erreur.
+    if -20 <= positionfinale_x_1 <= 20:
+        break
+    else:
+        print("Erreur: La position finale de la voiture 1 en x doit être entre -20m et 20m")
 
 while True:
-  position_y_1 = int(input("Indiquer la position de la voiture 1 en y entre -20m et 20m:"))
-  if -20 <= position_y_1 <= 20:
-    break
-  else:
-    print("Erreur: La position de la voiture 1 en y doit être entre -20m et 20m")
-  
+    position_y_1 = int(input("Indiquer la position de la voiture 1 en y entre -20m et 20m:"))
+    if -20 <= position_y_1 <= 20:
+        break
+    else:
+        print("Erreur: La position de la voiture 1 en y doit être entre -20m et 20m")
 
-#Voiture2
+
+while True:  # le loop fait en sorte que si l'utilisateur met une valeur hors de -20 et 20, ca affiche un message d'erreur tant qu'il met une valeur valide
+    position_x_1 = int(input(
+        "Indiquer la position de la voiture 1 en x entre -20m et 20m:"))  # Attention lorsque la position ne rentre pas dans les restrictions: Afficher erreur.
+    if -20 <= position_x_1 <= 20:
+        break
+    else:
+        print("Erreur: La position de la voiture 1 en x doit être entre -20m et 20m")
+
+while True:
+    position_y_1 = int(input("Indiquer la position de la voiture 1 en y entre -20m et 20m:"))
+    if -20 <= position_y_1 <= 20:
+        break
+    else:
+        print("Erreur: La position de la voiture 1 en y doit être entre -20m et 20m")
+
+# Voiture2
 vitesse_initiale2 = int(input("Entrer la vitesse (m/s) intiale du deuxième véhicule:"))
 masse_voiture2 = int(input("Entrer la masse (kg) du deuxième vehicule:"))
+acceleration_vehicule2 = int(input("Entrer l'acceleration du vehicule 2:"))
+vitesse_finale = int(input("Entrer la vitesse finale souhaité du vehicule 2:"))
 
 while True:
-  position_x_2 = int(input("Indiquer la position de la voiture 2 en x entre -20m et 20m:"))
-  if -20 <= position_x_2 <= 20:
-    break
-  else:
-    print("Erreur: La position de la voiture 2 en x doit être entre -20m et 20m")
+    position_x_2 = int(input("Indiquer la position de la voiture 2 en x entre -20m et 20m:"))
+    if -20 <= position_x_2 <= 20:
+        break
+    else:
+        print("Erreur: La position de la voiture 2 en x doit être entre -20m et 20m")
 
 while True:
-  position_y_2 = int(input("Indiquer la position de la voiture 2 en y entre -20m et 20m du deuxième vehicule:"))
-  if -20 <= position_y_2 <= 20:
-    break
-  else:
-    print("Erreur: La position de la voiture 2 en y doit être entre -20m et 20m")
+    position_y_2 = int(input("Indiquer la position de la voiture 2 en y entre -20m et 20m du deuxième vehicule:"))
+    if -20 <= position_y_2 <= 20:
+        break
+    else:
+        print("Erreur: La position de la voiture 2 en y doit être entre -20m et 20m")
+
